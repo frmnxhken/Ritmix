6f90934353a6a362920b7189348aa4218c42ede5
frmnxhken <frmnxhken@gmail.com>
2025-09-26 01:06:39 +0800

    Hold

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,7 +3,6 @@ import {
   NOTE_SIZE,
   LEAD_BEAT,
   MISS_WINDOW_MS,
-  CANVAS_HEIGHT,
 } from "@/utils/Constants.js";
 
 export default class Note {
@@ -47,34 +46,70 @@ export default class Note {
     this.time = noteData.time;
     const msPerBeat = (60 / meta.bpm) * 1000;
     this.leadTime = msPerBeat * LEAD_BEAT;
+    this.endTime = noteData.endTime || null;
+    this.isHit = false;
+    this.isHolding = false;
+    this.holdBroken = false;
+    this.tailY = this.y;
   }
 
   update(deltaTime, currentTimeMs) {
     const timeUntilHit = this.time - currentTimeMs;
-    // if (timeUntilHit < -200) {
-    //   return;
-    // }
-
     const travelPercentageRemaining = timeUntilHit / this.leadTime;
     const totalDistance = HIT_LINE_Y + this.size / 2;
     this.y = HIT_LINE_Y - totalDistance * travelPercentageRemaining;
+
+    if (this.endTime) {
+      const travelTail = (this.endTime - currentTimeMs) / this.leadTime;
+      this.tailY = HIT_LINE_Y - totalDistance * travelTail + this.size;
+    }
   }
 
   remove(currentTimeMs, score) {
-    if (this.isHit) return true;
-    if (currentTimeMs > this.time + MISS_WINDOW_MS) {
+    const checkTime = this.endTime || this.time;
+    if (currentTimeMs > checkTime + MISS_WINDOW_MS && !this.isHit) {
       score.update(0, 0);
       return true;
     }
 
-    if (this.y > CANVAS_HEIGHT + NOTE_SIZE) {
-      return true;
+    if (!this.endTime) {
+      if (this.isHit) {
+        return true;
+      }
+    }
+
+    if (this.endTime) {
+      if (currentTimeMs > this.endTime + MISS_WINDOW_MS) {
+        return true;
+      }
     }
 
     return false;
   }
 
   draw(ctx) {
+    if (this.endTime > this.time) {
+      const headY = this.y;
+      const tailY = this.tailY;
+
+      if (headY > -this.size) {
+        if (this.holdBroken) {
+          ctx.fillStyle = "#32495080";
+        } else if (this.isHolding) {
+          ctx.fillStyle = this.color;
+        } else {
+          ctx.fillStyle = `${this.color}80`;
+        }
+
+        ctx.fillRect(
+          this.x - this.size / 4,
+          tailY,
+          this.size / 2,
+          headY - tailY
+        );
+      }
+    }
+
     ctx.drawImage(
       this.image,
       this.x - this.size / 2,
